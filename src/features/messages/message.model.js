067aa5ae942ac39f2496ca95a9a4484ccf18c5f8
daitosakaki <daitosakaki@gmail.com>
2025-08-17 14:12:27 +0300

    Product

---
+++ src/features/messages/message.model.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+
+const messageSchema = new mongoose.Schema(
+  {
+    chatName: { type: String, trim: true },
+    isGroupChat: { type: Boolean, default: false },
+    members: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
+    lastMessage: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Message',
+    },
+    // Grup sohbetleri için admin bilgisi
+    groupAdmin: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+  },
+  { timestamps: true }
+);
+
+messageSchema.index({ members: 1 });
+
+const Message = mongoose.model('Chat', messageSchema);
+module.exports = Message;
